4cf598e90dabd30cb52dc1bce683c3f86184d366
PrimakMV <pm2710@mail.ru>
2025-01-16 17:24:04 +0300

    Added the basic project structure

---
+++ src/main/java/com/custom_annotations_demo/CustomAnnotationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.custom_annotations_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnnotationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnnotationsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custom_annotations_demo/CustomAnnotationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom_annotations_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnnotationsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
